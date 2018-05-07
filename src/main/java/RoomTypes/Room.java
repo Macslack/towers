f1c12e048a1ad222cbbdbd54f8a9b0be7edd1a17
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-05-07 11:44:40 +0100

    test

--- src/main/java/RoomTypes/Room.java
+++ src/main/java/RoomTypes/Room.java
@@ -34,7 +34,8 @@ public abstract class Room {
     }
 
     public ArrayList<Guest> getGuests() {
-        return guests;
+         ArrayList<Guest> copy = new ArrayList<>(guests);
+         return copy;
     }
     public void addGuest(Guest guest) {
         if (totalGuests() < room_capacity)
